e59a1328791855e9f6732c0ffa53c68cd22f0db8
Chokkapu leelavathi <chlieela921@gmail.com>
2023-08-23 11:08:13 +0000

    Congratulations

---
+++ index.js
@@ -0,0 +1,23 @@
+const element = (
+  <div className="main-container">
+    <div className="body-container">
+      <h1 className="heading">Congratulations </h1>
+      <div className="card-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+          className="logo-image"
+        />
+        <h1 className="heading">Kiran V</h1>
+        <p className="paragraph">
+          Vishnu institute of computer and TechTechnology Bhimavaram
+        </p>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+          className="logo-image1"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
